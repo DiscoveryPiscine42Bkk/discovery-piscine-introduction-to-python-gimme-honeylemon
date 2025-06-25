0eb3d1c617bd08bd7c5a53f47193d2c873f97066
gimme-honeylemon <67011734@kmitl.ac.th>
2025-06-25 03:10:52 +0000

    file uploading

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+    sys.exit()
+           
+i = 0
+while i < 11:
+    print(f"Table de {i}",end=" : ")
+    j = 0
+    while j < 11:
+        print(i * j, end=" ")
+        j += 1
+    print()
+    i += 1
